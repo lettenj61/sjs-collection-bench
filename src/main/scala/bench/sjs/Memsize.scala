301c0477dc8dab85c62955bc5e019a2f5d150a0d
lettenj61 <lettenj61@users.noreply.github.com>
2017-02-20 15:45:05 +0900

    add Streams in Memsize, also ported bench into FastOpt stage

--- src/main/scala/bench/sjs/Memsize.scala
+++ src/main/scala/bench/sjs/Memsize.scala
@@ -1,9 +1,24 @@
+package bench.sjs
+
 import scala.collection._
 import scala.scalajs.js
 import js.annotation._
 
 @JSExport
 object Memsize {
+
+  @JSExport
+  def main(): Unit = {
+    js.Dynamic.global.memsizeSnapshot = js.Dictionary(
+      "lists" -> lists,
+      "vectors" -> vectors,
+      "unforcedStreams" -> unforcedStreams,
+      "forcedStreams" -> forcedStreams,
+      "javaArrays" -> javaArrays,
+      "jsArrays" -> jsArrays
+    )
+  }
+
   def elem() = js.Dynamic.literal().asInstanceOf[js.Object]
 
   def samples[T](f: Int => T): js.Dictionary[Any] = {
@@ -12,22 +27,24 @@ object Memsize {
     d
   }
 
-  val Sizes = Seq(0, 1, 2, 4, 16, 64, 256, 1024, 4096, 16192)
+  def Sizes = Seq(0, 1, 2, 4, 16, 64, 256, 1024, 4096, 16192)
 
-  @JSExport
-  val lists = samples(i => List.fill(i)(elem()))
-  @JSExport
-  val vectors = samples(i => Vector.fill(i)(elem()))
+  def lists = samples(i => List.fill(i)(elem()))
+  def vectors = samples(i => Vector.fill(i)(elem()))
+  def unforcedStreams = samples(i => Stream.fill(i)(elem()))
+  def forcedStreams = samples({ i =>
+    val s = Iterator.fill(i)(elem()).toStream
+    s foreach (_ => ())
+    s
+  })
 
-  @JSExport
-  val javaArrays = samples({ i =>
+  def javaArrays = samples({ i =>
     val b = Array.newBuilder[js.Object]
     for (_ <- 0 until i) b += elem()
     b.result()
   })
 
-  @JSExport
-  val jsArrays = samples({ i =>
+  def jsArrays = samples({ i =>
     val xs = new js.Array[js.Object]()
     for (_ <- 0 until i) xs.push(elem())
     xs
